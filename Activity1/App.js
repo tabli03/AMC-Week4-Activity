427c9354976c6858a8be28cc8d8923280288cbee
tabli03 <arwyntablizo03@gmail.com>
2025-02-06 16:49:40 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import {Text, ScrollView} from 'react-native';
+
+const App = () => (
+      <ScrollView>
+        <Text>
+verse
+Hindi man araw-araw na nakangiti
+Ilang beses na rin tayong humihindi
+′Di na mabilang ang ating mga tampuhan
+Away-bati natin, 'di na namamalayan
+pre-chorus
+Eto tayo
+chorus
+Ngunit sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+Palagi
+verse
+Kung balikan man ang hirap, luha't lahat
+Ikaw ang paborito kong desisyon at
+'Pag napaligiran ng ingay at ng gulo
+′Di ko ′pagpapalit ngiti mo sa mundo
+pre-chorus
+Eto tayo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa 'yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako
+bridge
+Sa pagdating ng ating pilak at ginto
+Diyamante ma′y abutin, ikaw pa rin aking bituin
+Natatangi kong dalangin 'gang sa huling siglo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Mahal, sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+verse
+Hindi man araw-araw na nakangiti
+Ilang beses na rin tayong humihindi
+′Di na mabilang ang ating mga tampuhan
+Away-bati natin, 'di na namamalayan
+pre-chorus
+Eto tayo
+chorus
+Ngunit sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+Palagi
+verse
+Kung balikan man ang hirap, luha't lahat
+Ikaw ang paborito kong desisyon at
+'Pag napaligiran ng ingay at ng gulo
+′Di ko ′pagpapalit ngiti mo sa mundo
+pre-chorus
+Eto tayo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa 'yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako
+bridge
+Sa pagdating ng ating pilak at ginto
+Diyamante ma′y abutin, ikaw pa rin aking bituin
+Natatangi kong dalangin 'gang sa huling siglo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Mahal, sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+verse
+Hindi man araw-araw na nakangiti
+Ilang beses na rin tayong humihindi
+′Di na mabilang ang ating mga tampuhan
+Away-bati natin, 'di na namamalayan
+pre-chorus
+Eto tayo
+chorus
+Ngunit sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+Palagi
+verse
+Kung balikan man ang hirap, luha't lahat
+Ikaw ang paborito kong desisyon at
+'Pag napaligiran ng ingay at ng gulo
+′Di ko ′pagpapalit ngiti mo sa mundo
+pre-chorus
+Eto tayo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa 'yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako
+bridge
+Sa pagdating ng ating pilak at ginto
+Diyamante ma′y abutin, ikaw pa rin aking bituin
+Natatangi kong dalangin 'gang sa huling siglo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Mahal, sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+verse
+Hindi man araw-araw na nakangiti
+Ilang beses na rin tayong humihindi
+′Di na mabilang ang ating mga tampuhan
+Away-bati natin, 'di na namamalayan
+pre-chorus
+Eto tayo
+chorus
+Ngunit sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+Palagi
+verse
+Kung balikan man ang hirap, luha't lahat
+Ikaw ang paborito kong desisyon at
+'Pag napaligiran ng ingay at ng gulo
+′Di ko ′pagpapalit ngiti mo sa mundo
+pre-chorus
+Eto tayo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Hanggang sa huli, palagi
+Pipiliin kong maging sa 'yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako
+bridge
+Sa pagdating ng ating pilak at ginto
+Diyamante ma′y abutin, ikaw pa rin aking bituin
+Natatangi kong dalangin 'gang sa huling siglo
+chorus
+Sa huli, palagi
+Babalik pa rin sa yakap mo
+Mahal, sa huli, palagi
+Pipiliin kong maging sa ′yo
+chorus
+Ulit-ulitin man
+Nais kong malaman mong
+Iyo ako palagi
+        </Text>
+      </ScrollView>
+
+);
+
+export default App;
